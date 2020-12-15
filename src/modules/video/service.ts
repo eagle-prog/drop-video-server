0788f00f93b67be930b8f90a7cd1b9129202618c
Eagle19243 <eagle19243@gmail.com>
2020-12-16 04:10:08 +0800

    --no-sandbox

--- src/modules/video/service.ts
+++ src/modules/video/service.ts
@@ -8,14 +8,15 @@ export default class VideoService {
     private option:any = {
         ignoreDefaultArgs: [
             '--disable-extensions',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
             '--disable-infobars',
             '--window-position=0,0',
             '--ignore-certifcate-errors',
             '--ignore-certifcate-errors-spki-list',
             '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
             '--start-maximized',
+        ],
+        args: [
+            '--no-sandbox',
         ],
         headless: true,
     };
